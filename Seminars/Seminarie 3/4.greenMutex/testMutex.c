b619e27898483a8879c6114b87868e2b74935e2e
Adeel Hussain <adeelh@kth.se>
2021-12-15 20:05:34 +0100

    Finishing Seminar 3

--- Seminars/Seminarie 3/4.greenMutex/testMutex.c
+++ Seminars/Seminarie 3/4.greenMutex/testMutex.c
@@ -3,23 +3,27 @@
 
 int flag = 0;
 green_cond_t cond;
+green_mutex_t mutex;
+
 void *test(void *arg)
 {
 	int id = *(int *)arg;
-	int loop = 4;
+	int loop = 1000;
 	while (loop > 0)
 	{
-		if (flag == id)
+		printf("thread %d: %d\n", id, loop);
+		green_mutex_lock(&mutex);
+		
+		while (flag != id)
 		{
-			printf(" thread %d : %d\n", id, loop);
-			loop--;
-			flag = (id + 1) % 2;
-			green_cond_signal(&cond);
-		}
-		else
-		{
+			green_mutex_unlock(&mutex);
 			green_cond_wait(&cond);
+			green_mutex_lock(&mutex);
 		}
+		flag = (id + 1) % 2;
+		green_cond_signal(&cond);
+		green_mutex_unlock(&mutex);
+		loop--;
 	}
 }
 
